688c3339b5adb58f6bbe5b73de1efe82809a0cf1
yazva161 <yazva161@gmail.com>
2022-11-15 12:51:29 +0300

    Добавлен метод public static String calc(String input).

---
+++ Calculator_2/Main.java
@@ -0,0 +1,25 @@
+package Calculator_2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static String calc (String input){
+        NumbersArabicIsRoman nar = new NumbersArabicIsRoman();
+
+        try {
+            input = nar.resultExpr(input);
+
+        } catch (ArrayIndexOutOfBoundsException e){
+            throw new ArrayIndexOutOfBoundsException("Строка не является выражением.");
+        }
+        return input;
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Введите выражение с операндами от нуля до десяти: ");
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        s = Main.calc(s);
+    }
+}
